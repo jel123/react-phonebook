1abb3a6c44e25d01b951f902f706dd83f0f32148
jel123 <quinamotjeland@gmail.com>
2023-04-18 02:48:34 +0800

    Fixed

--- src/server/phonebook.js
+++ src/server/phonebook.js
@@ -3,19 +3,25 @@ import axios from "axios";
 const phoneBookUrl = process.env.BACKEND_API_URL;
 
 const getPhonebookData = () => {
-  return axios.get(phoneBookUrl).then((res) => res.data);
+  return axios.get(phoneBookUrl, { crossDomain: true }).then((res) => res.data);
 };
 
 const addPhoneBook = (phoneObj) => {
-  return axios.post(phoneBookUrl, phoneObj).then((res) => res);
+  return axios
+    .post(phoneBookUrl, { crossDomain: true }, phoneObj)
+    .then((res) => res);
 };
 
 const alterPhoneBook = (phoneObj, id) => {
-  return axios.put(`${phoneBookUrl}/${id}`, phoneObj).then((res) => res.data);
+  return axios
+    .put(`${phoneBookUrl}/${id}`, { crossDomain: true }, phoneObj)
+    .then((res) => res.data);
 };
 
 const deleteContact = (id) => {
-  return axios.delete(`${phoneBookUrl}/${id}`).then((res) => res.data);
+  return axios
+    .delete(`${phoneBookUrl}/${id}`, { crossDomain: true })
+    .then((res) => res.data);
 };
 
 const exportedObject = {
